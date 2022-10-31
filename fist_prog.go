7c83c12fb83675b84180362db20f0db808b7692f
GipeR <vicommsk@gmail.com>
2022-10-31 22:07:39 +0400

    2 lessons

--- fist_prog.go
+++ fist_prog.go
@@ -1,9 +1,32 @@
+//package main
+//
+//import "fmt"
+//
+//// this is a comment
+//
+//func main() {
+//	fmt.Println(len("Hello World"))
+//	fmt.Println("Hello World"[1])
+//	fmt.Println("Hello" + "World")
+//}
+
+//package main
+//
+//import "fmt"
+//
+//func main() {
+//	fmt.Println(true && true)
+//	fmt.Println(true && false)
+//	fmt.Println(true || true)
+//	fmt.Println(true || false)
+//	fmt.Println(!true)
+//}
+
 package main
 
 import "fmt"
 
-// this is a comment
-
 func main() {
-	fmt.Println("Hello World123")
+	fmt.Println(false || false || true)
+
 }
